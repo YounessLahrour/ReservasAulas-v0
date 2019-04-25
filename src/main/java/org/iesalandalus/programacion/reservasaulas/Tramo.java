cd3130f444ed5acb7749d9cfd5bbb098f99207f0
Youness <Youness@DESKTOP-BQ87NOV>
2019-04-26 00:29:28 +0200

    Ejer 3. Creo la clase Enum MANANA y TARDE

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Tramo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.reservasaulas;
+
+/**
+ *
+ * @author Youness
+ */
+public enum Tramo {
+    MANANA, TARDE;
+    
+}
