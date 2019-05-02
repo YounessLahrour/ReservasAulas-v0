a2e4a96f78e7ed014bb292569d78aeae38eb48fb
Youness <Youness@DESKTOP-BQ87NOV>
2019-05-02 03:05:17 +0200

    Ejer 14. creo la clase MainApp.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,9 +1,14 @@
 package org.iesalandalus.programacion.reservasaulas;
 
+import javax.naming.OperationNotSupportedException;
+import org.iesalandalus.programacion.reservasaulas.vista.IUTextual;
+
 public class MainApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws OperationNotSupportedException {
 		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
+                IUTextual iniciarPrograma = new IUTextual();
+		iniciarPrograma.comenzar();
 	}
 
 }
